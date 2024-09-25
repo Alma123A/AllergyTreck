aba7a1d9890a7989126768cafaab8aed26922e2f
Alma123A <alma.tisch1@gmail.com>
2024-09-25 22:44:47 +0300

    first commit and push

---
+++ app/src/main/java/com/example/trackallergy/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.trackallergy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.trackallergy.ui.theme.TrackAllergyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+}
